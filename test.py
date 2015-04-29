49054bd405b5578fbb39957990406085c128e024
Wojtek <wojtekpietrucha@gmail.com>
2015-04-29 20:03:59 +0200

    Added rozwiazania.py

---
+++ test.py
@@ -0,0 +1,15 @@
+__author__ = 'kuban'
+
+
+print("Janusz")
+
+
+def costam(N):
+    print("to jest N:"+str(N))
+
+L=[1,2,"aa","aa",5,]
+for elem in L:
+    costam(elem)
+    print("Janusz")
+    print(5+elem)
+
